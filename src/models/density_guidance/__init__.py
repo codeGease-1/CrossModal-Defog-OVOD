6c72da7b2ec1ea8be745e5b86ab348d837595433
chen <3501799704@qq.com>
2026-09-03 15:48:13 +0800

    1

--- src/models/density_guidance/__init__.py
+++ src/models/density_guidance/__init__.py
@@ -4,9 +4,9 @@ Density Guidance 模块
 包含密度图引导注意力模块。
 
 子模块:
-    - density_guidance: DensityGuidanceModule
+    - density_guidance: DensityGuidanceModule, create_density_guidance_modules
 """
 
-from .density_guidance import DensityGuidanceModule
+from .density_guidance import DensityGuidanceModule, create_density_guidance_modules
 
-__all__ = ["DensityGuidanceModule"]
+__all__ = ["DensityGuidanceModule", "create_density_guidance_modules"]
